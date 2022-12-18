0f7bab2f26f37ddd3b89c65894b4b1ff31569e25
Nuno Guterres Nogueira <guterresnogueira@gmail.com>
2022-12-18 19:05:48 +0000

    Added Doxygen comments to the MachineRoom class, added brake method and simplified /fixed once and for all the motor controller code.

--- firmware/include/MachineRoom.h
+++ firmware/include/MachineRoom.h
@@ -5,6 +5,9 @@ class MachineRoom {
   public:
     MachineRoom(uint8_t leftMotorIN1, uint8_t leftMotorIN2, uint8_t rightMotorIN1, uint8_t rightMotorIN2);
     void installLogger(Stream *serial);
+    void forward(uint8_t pwm);
+    void backwards(uint8_t pwm);
+    void brake();
     void update(int x, int y);
   private:
     Stream *serial;
--- firmware/src/MachineRoom.cpp
+++ firmware/src/MachineRoom.cpp
@@ -1,62 +1,92 @@
 #include "MachineRoom.h"
 
+/// @brief Initializes the Machine Room (aka Wheels Controller)
+/// @param leftMotorIN1 as digital enabled PWM forward pin
+/// @param leftMotorIN2 as digital enabled PWM reverse pin
+/// @param rightMotorIN1 as digital enabled PWM forward pin
+/// @param rightMotorIN2 as digital enabled PWM reverse pin
 MachineRoom::MachineRoom(
   uint8_t leftMotorIN1,
   uint8_t leftMotorIN2,
   uint8_t rightMotorIN1,
-  uint8_t rightMotorIN2) : left(leftMotorIN1, leftMotorIN2), right(rightMotorIN1, rightMotorIN2) {
+  uint8_t rightMotorIN2) :
+    left(leftMotorIN1, leftMotorIN2),
+    right(rightMotorIN1, rightMotorIN2) {
     #ifdef ESP8266
     // Only available on the ESP8266 Arduino Core
     analogWriteRange(MOTOR_PWM_RANGE);
     #endif
   }
 
+/// @brief Store the stream object so it can be used to debug code
+/// whenever needed.
+/// @param serial Serial object of stream type
 void MachineRoom::installLogger(Stream *serial) {
   this->serial = serial;
 }
 
+/// @brief Whenever the car needs to just go forward
+/// @param pwm as an unsigned char from 0 to 100
+void MachineRoom::forward(uint8_t pwm) {
+  right.forward(pwm);
+  left.forward(pwm);
+}
+
+/// @brief Whenever the car needs to just go backwards
+/// @param pwm as an unsigned char from 0 to 100
+void MachineRoom::backwards(uint8_t pwm) {
+  right.reverse(pwm);
+  left.reverse(pwm);
+}
+
+/// @brief Whenever the car needs to break
+/// @note Not quite sure this is the right way to do it: either pull-up or
+/// pull-down.
+void MachineRoom::brake() {
+  right.update(MOTOR_PWM_RANGE, MOTOR_PWM_RANGE);
+  left.update(MOTOR_PWM_RANGE, MOTOR_PWM_RANGE);
+}
+
 void MachineRoom::update(int x, int y) {
+  // Whenever the joystick returns to zero, stop.
+  // TODO: include a setting to disable this if necessary.
+  if (x == 0 && y == 0) {
+    this->brake();
+    return;
+  }
+
   uint8_t backwards = ((uint32_t) y) >> 31;
   uint8_t goingLeft = ((uint32_t) x) >> 31;
-  uint8_t filteredY = abs(y) & MOTOR_MASK; // Limited to 100;
-  uint8_t filteredX = abs(x) & MOTOR_MASK; // Limited to 100;
+  // The Y axis informs us the power we should provide to the motors, whereas
+  // the X axis informs us about the direction the car should go.
+  uint8_t absoluteY = abs(y) & MOTOR_MASK; // Limited to 100;
+  uint8_t absoluteX = abs(x) & MOTOR_MASK; // Limited to 100;
 
-  uint8_t motionLimit = MOTOR_JOYSTICK_THRESHOLD;
+  // Whenever the X axis is below the motion threshold, the car should drive
+  // straight.
+  if (absoluteX < MOTOR_JOYSTICK_THRESHOLD) {
+    backwards ? this->backwards(absoluteY) : this->forward(absoluteY);
+    return;
+  }
 
-  if (backwards) {
-    if (filteredX < motionLimit) {
-      left.reverse(filteredY);
-      right.reverse(filteredY);
-    } else if (filteredY > motionLimit) {
-      if (goingLeft) {
-        // Turning left.
-        int mappedLeft = filteredY - filteredX;
-        right.reverse(filteredY);
-        mappedLeft > 0 ? left.reverse(abs(mappedLeft)) : left.forward(abs(mappedLeft));
-      } else {
-        // Turning right.
-        int mappedRight = filteredY + filteredX;
-        left.reverse(filteredY);
-        mappedRight > 0 ? left.reverse(abs(mappedRight)) : left.reverse(abs(mappedRight));
-      }
-    }
-  } else {
-    // Car is going forward.
-    if (filteredX < motionLimit) {
-      left.forward(filteredY);
-      right.forward(filteredY);
-    } else if (filteredY > motionLimit) {
-      if (goingLeft) {
-        // Turning left.
-        int mappedLeft = filteredY + filteredX;
-        right.forward(filteredY);
-        mappedLeft > 0 ? left.forward(abs(mappedLeft)) : left.reverse(abs(mappedLeft));
-      } else {
-        // Turning right.
-        int mappedRight = filteredY - filteredX;
-        left.forward(filteredY);
-        mappedRight > 0 ? right.forward(abs(mappedRight)) : right.reverse(abs(mappedRight));
-      }
-    }
+  // We split the quadrant in two halfs using `absoluteY > absoluteX`.
+  // First half is the one closer to the X axis and the second to the Y axis.
+  int diff = absoluteY > absoluteX ? absoluteY - absoluteX : absoluteX - absoluteY;
+  if (!goingLeft && !backwards) {
+    // 1st quadrant - forward and right.
+    left.forward(absoluteY);
+    absoluteY > absoluteX ? right.forward(diff) : right.reverse(diff);
+  } else if (goingLeft && !backwards) {
+    // 2nd quadrant - forward and left.
+    right.forward(absoluteY);
+    absoluteY > absoluteX ? left.forward(diff) : left.reverse(diff);
+  } else if (goingLeft && backwards) {
+    // 3rd quandrant - backwards and left.
+    right.reverse(absoluteY);
+    absoluteY > absoluteX ? left.reverse(diff) : left.forward(diff);
+  } else if (!goingLeft && backwards) {
+    // 4th quadrant - backwards and right.
+    left.reverse(absoluteY);
+    absoluteY > absoluteX ? right.reverse(diff) : right.forward(diff);
   }
 }
