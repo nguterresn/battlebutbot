1b0bfc227f848b67facb0b4732143ea6feebefee
Gustaf Edén <gustaf@kisi.io>
2023-06-21 00:51:47 +0200

    transmitt MPU6050 data

--- transmitter/src/main.cpp
+++ transmitter/src/main.cpp
@@ -9,10 +9,19 @@
   copies or substantial portions of the Software.
 */
 
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "Wire.h"
+
+
+
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
-#define TRANSMITTER
+// #define TRANSMITTER
 
 // REPLACE WITH THE MAC Address of your receiver 
 
@@ -24,6 +33,45 @@ uint8_t broadcastAddress[] = {0x3C, 0x61, 0x05, 0xD2, 0xB6, 0x79};
 uint8_t broadcastAddress[] = {0xBC, 0xDD, 0xC2, 0x24, 0x7F, 0x78};
 #endif
 
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+// #define OUTPUT_READABLE_WORLDACCEL
+
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+#define INTERRUPT_PIN 15 // use pin 15 on ESP8266
+
+const char DEVICE_NAME[] = "mpu6050";
+
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void ICACHE_RAM_ATTR dmpDataReady() {
+    mpuInterrupt = true;
+}
+
 // Define variables to store DHT readings to be sent
 unsigned long ms;
 
@@ -31,7 +79,7 @@ unsigned long ms;
 unsigned long incomingMillis;
 
 // Updates DHT readings every 10 seconds
-const long interval = 1000; 
+const long interval = 2; 
 unsigned long previousMillis = 0;    // will store last time DHT was updated 
 
 // Variable to store if sending data was successful
@@ -41,6 +89,7 @@ String success;
 //Must match the receiver structure
 typedef struct struct_message {
     unsigned long millis;
+    float ypr[3];
 } struct_message;
 
 // Create a struct_message called DHTReadings to hold sensor readings
@@ -48,6 +97,8 @@ struct_message foo;
 
 // Create a struct_message to hold incoming sensor readings
 struct_message incomingFoo;
+
+float incomingYPR[3];
 
 void sendMillis(unsigned long ms) {
   foo.millis = ms;
@@ -69,19 +120,78 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
 
 // Callback when data is received
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
-  memcpy(&incomingFoo, incomingData, sizeof(incomingFoo));
-  incomingMillis = incomingFoo.millis;
+  memcpy(&incomingYPR, incomingData, sizeof(incomingYPR));
+  // incomingMillis = incomingFoo.millis;
   #ifndef TRANSMITTER
-  sendMillis(incomingFoo.millis);
+  // sendMillis(incomingFoo.millis);
   #endif
-  Serial.printf("micros rec: %lu\n", incomingMillis);
-  unsigned long res = micros() - incomingMillis;
-  Serial.printf("res: %lu\n", res);
+  // Serial.printf("micros rec: %lu\n", incomingMillis);
+  // unsigned long res = micros() - incomingMillis;
+  // Serial.printf("res: %lu\n", res);
+
+    Serial.print(incomingYPR[1] * 180/M_PI);
+    Serial.print("\t");
+    Serial.println(incomingYPR[2] * 180/M_PI);
+}
+
+
+void mpu_setup()
+{
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
 }
 
 void setup() {
   // Init Serial Monitor
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.println(F("\nOrientation Sensor OSC output")); Serial.println();
 
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
@@ -97,24 +207,82 @@ void setup() {
   // Set ESP-NOW Role
   esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
+  // // Once ESPNow is successfully Init, we will register for Send CB to
+  // // get the status of Trasnmitted packet
   esp_now_register_send_cb(OnDataSent);
   
-  // Register peer
+  // // Register peer
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
   
-  // Register for a callback function that will be called when data is received
+  // // Register for a callback function that will be called when data is received
   esp_now_register_recv_cb(OnDataRecv);
+
+  mpu_setup();
 }
  
+
+
+void mpu_loop()
+{
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+
+  // wait for MPU interrupt or extra packet(s) available
+  if (!mpuInterrupt && fifoCount < packetSize) return;
+
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & 0x02) {
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+    // read a packet from FIFO
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+    // display Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    Serial.print("ypr\t");
+    // Serial.print(ypr[0] * 180/M_PI);
+    // Serial.print("\t");
+    Serial.print(ypr[1] * 180/M_PI);
+    Serial.print("\t");
+    Serial.println(ypr[2] * 180/M_PI);
+
+
+    esp_now_send(broadcastAddress, (uint8_t *) &ypr, sizeof(ypr));
+#endif
+
+  }
+}
 void loop() {
   #ifdef TRANSMITTER
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     // save the last time you updated the DHT values
     previousMillis = currentMillis;
-    sendMillis(micros());
+    mpu_loop();
+    // sendMillis();
   }
   #endif
+
+  // mpu_loop();
 }
