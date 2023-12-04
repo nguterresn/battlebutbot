f7254576877e5e91171b137bdc3207b4c621fd7b
Gustaf Edén <gustaf@kisi.io>
2023-12-04 22:33:16 +0100

    realise the oled I have is 128x64 lol

--- transmitter/include/oled.h
+++ transmitter/include/oled.h
@@ -7,7 +7,7 @@
 #include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH   128        // OLED display width, in pixels
-#define SCREEN_HEIGHT  32         // OLED display height, in pixels
+#define SCREEN_HEIGHT  64         // OLED display height, in pixels
 #define OLED_RESET     -1         // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C       ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 
